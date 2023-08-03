7b99aa06155c1de26915d3bb3757f2e46e86addb
Aining Li <annieli2025@u.northwestern.edu>
2023-08-03 16:14:02 +0800

    01实现切换场景

--- Assets/Scripts/Transition/Teleport.cs
+++ Assets/Scripts/Transition/Teleport.cs
@@ -7,4 +7,9 @@ public class Teleport : MonoBehaviour
     // 用到 M tools（封装好的工具），将string转化成列表可以勾选场景，避免输错
     [SceneName] public string sceneFrom;
     [SceneName] public string sceneTo;
+
+    public void TeleportToScene()
+    {
+        TransitionManager.Instance.Transition(sceneFrom, sceneTo);
+    }
 }
--- Assets/Scripts/Transition/TransitionManager.cs
+++ Assets/Scripts/Transition/TransitionManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TransitionManager : Singleton<TransitionManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration;
+    private bool isFade;
+    public void Transition(string from, string to)
     {
-        
+        if (!isFade)
+        {
+            StartCoroutine(TransitionToScene(from, to));
+        }       
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TransitionToScene(string from, string to)
     {
-        
+        yield return Fade(1);
+        yield return SceneManager.UnloadSceneAsync(from);
+        yield return SceneManager.LoadSceneAsync(to, LoadSceneMode.Additive);
+
+        // 设置新场景为激活场景
+        Scene newScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+        SceneManager.SetActiveScene(newScene);
+
+        yield return Fade(0);
+    }
+
+    /// <summary>
+    /// 淡入淡出场景
+    /// </summary>
+    /// <param name="targetAlpha">1是黑，0是透明</param>
+    /// <returns></returns>
+    private IEnumerator Fade(float targetAlpha)
+    {
+        isFade = true;
+        fadeCanvasGroup.blocksRaycasts = true;
+        float speed = Mathf.Abs(fadeCanvasGroup.alpha - targetAlpha) / fadeDuration;
+        while (!Mathf.Approximately(fadeCanvasGroup.alpha, targetAlpha))
+        {
+            fadeCanvasGroup.alpha = Mathf.MoveTowards(fadeCanvasGroup.alpha, targetAlpha, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        fadeCanvasGroup.blocksRaycasts = false;
+        isFade = false;
     }
 }
